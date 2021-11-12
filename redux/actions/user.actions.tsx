efcfb008d2721785b774f4f3a44ffeec57118774
porkytheblack <porkytheblack@gmail.com>
2021-11-12 07:58:39 +0000

    .

--- redux/actions/user.actions.tsx
+++ redux/actions/user.actions.tsx
@@ -1,8 +1,14 @@
+import { AnyTypeAnnotation } from "@babel/types";
 import { AxiosResponse } from "axios";
 import { useDispatch } from "react-redux";
 import { ActionCreator } from "redux";
 import { create_user } from "../../services/UserServices";
 import {  set_token, set_user,  tdelete_token,  tedit_user,  tupdate_token, UserActionTypes } from "../types/Index";
+
+interface error_message {
+    status: number;
+    details: any;
+}
 
 const dispatch = useDispatch()
 
@@ -40,14 +46,17 @@ export const delete_token: ActionCreator<UserActionTypes> = ()=>{
     }
 }
 
-export const user_create = (user: any)=>{
+export const user_create = (user: any): void | error_message=>{
     create_user(user).then((res: AxiosResponse)=>{
         if(res.status == 200){
             dispatch(create_user({
                 ...res.data
             }))
         }else{
-
+            return {
+                status: res.status,
+                details: res.data
+            }
         }
     })
 }
